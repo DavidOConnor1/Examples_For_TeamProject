5526a2785af5eccf32a0af0ac68f85b5f3e7f45a
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-04-12 16:16:18 +0100

    I have finished creating the code to write to a json file and then read from a json file

--- User-Authentication Login/server.js
+++ User-Authentication Login/server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-
+const fs = require('fs');
 
 
 const app = express();
 app.use(express.json());
-const users = []
+let users = []
+
 
 app.get('/users', (req, res) => {
+    try {
+        users = JSON.parse(fs.readFileSync('users.json', 'utf8'));
+    } catch (error) {
+        console.error(error);
+        res.status(500).send();
+        return;
+    }
+
     res.json(users);
 });
 
@@ -18,16 +27,47 @@ app.post('/users', async (req, res) => {
         console.log(salt);
         console.log(hashedPassword);
         const user = {name:  req.body.name, password: hashedPassword};
+
+        
+        // Read users from the file
+        try {
+            users = JSON.parse(fs.readFileSync('users.json', 'utf8'));
+        } catch (error) {
+            // If file does not exist, ignore the error
+            if (error.code !== 'ENOENT') {
+                console.error(error);
+                res.status(500).send();
+                return;
+            }
+        }
+
+        // Add the new user
         users.push(user);
+
+        // Write the updated users back to the file
+        try {
+            fs.writeFileSync('users.json', JSON.stringify(users), 'utf8');
+        } catch (error) {
+            console.error(error);
+            res.status(500).send();
+            return;
+        }
+
         res.status(201).send();
     } catch(error){
         res.status(500).send();
         console.log(error);
     }
-   
 });
 
 app.post('/users/login', async (req, res) => {
+    try {
+        users = JSON.parse(fs.readFileSync('users.json', 'utf8'));
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Cannot find users file');
+        return;
+    }
     const user = users.find(user => user.name = req.body.name);
 
     if(user ==null){
